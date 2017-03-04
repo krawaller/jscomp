d228a526382038bc0b4f8fe932b0da1ab42d541c
David Waller <david@krawaller.se>
2017-03-04 11:28:32 +0100

    comparison functionality

--- pages/scripts/code.js
+++ pages/scripts/code.js
@@ -5,16 +5,24 @@ if ($drop.length){
     $("#codeselector").attr("style","");
     $(".codelink").attr("style","");
     $(".codeheadline").hide();
+    $("<option>").attr("value","NONE").text(" --- Choose another implementation --- ").appendTo($drop);
     $(".othercode").each(function(idx,elem){
         var name = $(elem).attr("data-name");
         $("<option>").attr("value",name).text(name+":").appendTo($drop);
-        if(idx){
-            $(elem).hide();
-        }
+        $(elem).hide();
     });
     $drop.on("change",function(){
         var target = $drop.val();
-        $(".othercode").hide();
-        $(".othercode[data-name='"+target+"']").show();
+        if (target === "NONE"){
+            $(".sidebyside").removeClass("comparing");
+        } else {
+            $(".othercode").hide();
+            $(".othercode[data-name='"+target+"']").show();
+            $(".sidebyside").addClass("comparing");
+        }
+    });
+    $(".hider").on("click",function(){
+        $drop.val("NONE");
+        $(".sidebyside").removeClass("comparing");
     });
 }
